7f00e7cf32b426abfa31aaf85133fce62858c340
achouliaras <andreas.chouliaras@ucdconnect.ie>
2025-01-29 18:50:58 +0000

    minor improvements in offline pretraining

--- learning_offline/pretraining.py
+++ learning_offline/pretraining.py
@@ -93,6 +93,7 @@ class Workspace(object):
             next_obs, reward, terminated, truncated, info = self.env.step(action)
 
             done = terminated or truncated
+            done = terminated or truncated
             if terminated or truncated:
                 episode_time = time.time() - start_time
                 self.total_time += episode_time
@@ -102,10 +103,7 @@ class Workspace(object):
                 next_obs, _ = self.env.reset()
                 # next_obs, _, _, _, _ = self.env.step(1) # FIRE action for breakout
             
-            self.obs[global_step]=obs
-            self.actions[global_step]=action
-            self.rewards[global_step]=reward
-            self.dones[global_step]=done
+            self.trajectory.append([obs,action,reward,done,next_obs]) # Pre allocate memory for all data
             obs = next_obs
 
         print(f'DATA GENERATED: Steps:{global_step+1}, Episodes:{self.episode}, Time:{self.total_time} sec')
